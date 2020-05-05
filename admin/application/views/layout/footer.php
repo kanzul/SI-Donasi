71f12d7ffee2e5e13b946b2c7c46b463301df980
kanzul <kanzulaufa3@gmail.com>
2020-05-05 16:07:42 +0700

    first commit

---
+++ admin/application/views/layout/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer dark-bg">
+    <div class="pull-right hidden-xs"></div>
+     Copyright &copy; 2019 Lembaga Manajemen Infaq 
+    
+    <div class="col-md-6 text-right">
+     <b>Created by KP ITATS Team</b> |
+    </div>
+</footer>
